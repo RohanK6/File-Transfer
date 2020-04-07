732c3eb938bc99fa338920ed4e8acfec560ca284
Rohan <rohan.khanderia@gmail.com>
2020-04-07 00:46:03 -0400

    Customized Django admin page

--- transfer/admin.py
+++ transfer/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import File
 
 # Register your models here.
-admin.site.register(File)
+class FileAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('File Name', {'fields': ['fileName']}),
+        ('File Description', {'fields': ['description']}),
+        ('Upload Date', {'fields': ['dateUploaded']})
+    ]
+
+    list_display = ('fileName', 'description', 'dateUploaded')
+    list_filter = ['dateUploaded']
+    search_fields = ['fileName']
+
+admin.site.register(File, FileAdmin)
