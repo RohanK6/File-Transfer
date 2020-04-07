85e7d8d4c1658faf6c5ce8e3e0b3808483c109e5
Rohan <rohan.khanderia@gmail.com>
2020-04-07 00:21:24 -0400

    Created separate pages for the home, upload, and download pages

--- transfer/urls.py
+++ transfer/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('/upload', views.upload, name='upload'),
+    path('/download', views.download, name='download')
 ]
--- transfer/views.py
+++ transfer/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader 
+
+from .models import File
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hi")
+    return render(request, 'transfer/index.html')
+
+def upload(request):
+    return render(request, 'transfer/upload.html')
+
+def download(request):
+    return render(request, 'tranafer/download.html')
